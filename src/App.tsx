320d1fc9b5e96c9f17329db136aca0f3283bf5f2
Dan Feldman <danfeldman89@gmail.com>
2024-05-22 19:09:00 +0300

    MUI table added with server connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
-import React, { FunctionComponent, useState, useEffect, useMemo } from 'react';
+import React, { FunctionComponent, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import DbDisplayTable from './components/DbDisplayTable/DbDisplayTable';
 
-interface IProps {}
+interface IProps {
+}
 
 type status = 'WORKING' | 'NOT WORKING' | 'LOADING'
 
 const statusInfo = {
   'WORKING': {
     statusMessage: 'Fake server is working',
-    statusColor: 'green',
+    statusColor: 'green'
   },
   'NOT WORKING': {
     statusMessage: 'Fake server is not working',
-    statusColor: 'red',
+    statusColor: 'red'
   },
   'LOADING': {
     statusMessage: 'Loading...',
-    statusColor: 'yellow',
-  },
+    statusColor: 'yellow'
+  }
 };
+
+export type DatabaseType = 'snowflake' | 'trino' | 'mySql';
+
+export interface DatabaseDescriptor {
+  id: string,
+  name: string,
+  url: string,
+  username: string,
+  password: string,
+  type: DatabaseType,
+}
 
 const App: FunctionComponent<IProps> = () => {
   const [serverStatus, setServerStatus] = useState<status>('LOADING');
+  const [databaseList, setDatabaseList] = useState<DatabaseDescriptor[] | undefined>(undefined);
 
   const { statusMessage, statusColor } = useMemo(() => statusInfo[serverStatus], [serverStatus]);
 
@@ -29,13 +43,15 @@ const App: FunctionComponent<IProps> = () => {
     const fakeServerUrl = 'http://localhost:4000/databases';
 
     axios.get(fakeServerUrl)
-      .then((response) => {
-        setServerStatus('WORKING');
-        console.log(response.data);
-      })
-      .catch(() => {
-        setServerStatus('NOT WORKING');
-      });
+         .then((response) => {
+           setServerStatus('WORKING');
+           let data = response.data;
+           console.log(data);
+           setDatabaseList(data);
+         })
+         .catch(() => {
+           setServerStatus('NOT WORKING');
+         });
   }, []);
 
   return (
@@ -43,8 +59,10 @@ const App: FunctionComponent<IProps> = () => {
       <h5 style={{ color: statusColor }}>
         {statusMessage}
       </h5>
+
+      <DbDisplayTable databaseList={databaseList}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/DbDisplayTable/DbDisplayTable.tsx
@@ -0,0 +1,162 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { DatabaseDescriptor } from "../../App";
+
+const columns: GridColDef[] = [
+  { field: 'dbName', headerName: 'Database Name', width: 70 },
+  { field: 'username', headerName: 'Username', width: 130 },
+  { field: 'dbType', headerName: 'Database Type', width: 130 }
+];
+
+interface IProps {
+  databaseList?: DatabaseDescriptor[];
+}
+
+export default function DataTable({ databaseList }: IProps) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  function addDbClick() {
+
+  }
+
+  function handleClose() {
+    setDialogOpen(false);
+  }
+
+  function submitForm() {
+    console.log('form submitted');
+  }
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={databaseList === undefined ? [] : databaseList?.map(value => {
+          return {
+            id: value.id,
+            dbName: value.name,
+            username: value.username,
+            dbType: value.type
+          };
+        })}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 }
+          }
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        slots={{}} // TODO: impl footer here
+      />
+
+      <Button
+        variant="contained"
+        color="secondary"
+        // onClick={handleDelete}
+        // disabled={selectedRows.length === 0}
+        style={{ marginTop: 20 }}
+      >
+        Delete Selected
+      </Button>
+
+      <Button
+        variant="contained"
+        color="secondary"
+        // disabled={selectedRows.length === 0}
+        style={{ marginTop: 20 }}
+        onClick={() => openDialog()}
+      >
+        Add Database
+      </Button>
+
+      {/*<AddDbDialog open={dialogOpen} />*/}
+      <form onSubmit={() => submitForm()}>
+        <Dialog
+          open={dialogOpen}
+          onClose={handleClose}
+          PaperProps={{
+            component: 'form',
+            onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+              event.preventDefault();
+              const formData = new FormData(event.currentTarget);
+              const formJson = Object.fromEntries((
+                                                    formData as any).entries());
+              const email = formJson.email;
+              console.log(email);
+              handleClose();
+            }
+          }}
+        >
+          <DialogTitle>Add Database Connection Details</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="database_name"
+              label="Database Name"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="url"
+              name="url"
+              label="URL"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="name"
+              label="Name"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="user-password"
+              name="user-password"
+              label="User Password"
+              type="text"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="type"
+              name="type"
+              label="Type(Snowflake,Trino,MySQL)"
+              type="email" //TODO:Input
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => handleClose()}>Cancel</Button>
+            <Button type="submit">Subscribe</Button>
+          </DialogActions>
+        </Dialog>
+      </form>
+    </div>
+  );
+
+  function openDialog() {
+    setDialogOpen(true);
+  }
+}
